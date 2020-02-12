3429cbb0735bcf69489ae62a61b8157af6af362f
CrazyMax <crazy-max@users.noreply.github.com>
2020-02-12 23:01:34 +0100

    Add vscodium.cmd (#4)

---
+++ res/vscodium.cmd
@@ -0,0 +1,12 @@
+@echo off
+setlocal
+
+set VSCODE_APPDATA=%~dp0data\appdata
+set VSCODE_LOGS=%~dp0data\logs
+set VSCODE_EXTENSIONS=%~dp0data\extensions
+
+set VSCODE_DEV=
+set ELECTRON_RUN_AS_NODE=1
+
+call "%~dp0app\VSCodium.exe" "%~dp0app\resources\app\out\cli.js" %*
+endlocal
